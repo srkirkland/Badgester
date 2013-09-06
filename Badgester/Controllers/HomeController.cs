3ae0f6f826e301d06ec6bc3e05d791195e1ac783
Scott Kirkland <srkirkland@gmail.com>
2013-09-06 10:46:14 -0700

    simple issuer script load and attempt to issue

---
+++ Badgester/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Badgester.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Verify(string id)
+        {
+            using (var client = new SmtpClient("smtp.ucdavis.edu"))
+            {
+                client.Send("srkirkland@ucdavis.edu", "srkirkland@ucdavis.edu", "Attempt to verify badge assertion",
+                            "mozilla called for the verification");
+            }
+            
+            return Content("Testing123");
+        }
+    }
+}
